776bbf8c23a6b37ee023370cd0fada0738b69708
Danilo <omaracevedo785@gmail.com>
2025-02-11 22:04:10 +0100

    Commit modificacion funcion agregar amigos

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
+let listaAmi = [];
 
 
-function addfriend(){
-    let input= document.querySelectorById("amigo")
+function agregarAmigo(){
+    let input= document.getElementById("amigo")
     let amigo = input.value.trim();
 
     // Validar la entrada
@@ -14,13 +14,39 @@ function addfriend(){
     }
 
     // Actualizar el array de amigos
-    amigos.push(amigo);
+    listaAmi.push(amigo);
 
     // Limpiar el campo de entrada
     input.value= "";
 
+    //Actualizar la lista en el Html
+    updateList()
 }
 
 function updateList(){
-    
+    let list = document.getElementById("listaAmigos");
+    list.innerHTML= "";
+    listaAmigos.forEach(friend =>{
+        let li = document.createElement("li");
+        li.textContent=friend;
+        list.appendChild(li);
+    });
+}
+
+function sortearAmigo(){
+
+    //Validar que la lista de amigos no esté vacía
+    if(listaAmi.length===0){
+        alert("No hay amigos en la lista para sortear");
+        return;
+    }
+    //Generar indice aleatorio
+    let randomIndex=Math.floor(Math.random()* listaAmi.length);
+
+    //Obterner el nombre sorteado
+    let selectedFriend = listaAmi[randomIndex];
+
+    // Mostrar el resultado
+    document.getElementById("resultado").innerHTML = "Amigo seleccionado: " + selectedFriend;
+
 }
